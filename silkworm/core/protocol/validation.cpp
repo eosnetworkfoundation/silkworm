aa54a5150646ee791a7e0dcbc8075b6011b3c53c
kayan <taokayan13@gmail.com>
2025-01-09 19:47:22 +0800

    homestead fix

--- silkworm/core/protocol/validation.cpp
+++ silkworm/core/protocol/validation.cpp
@@ -66,7 +66,11 @@ ValidationResult pre_validate_transaction(const Transaction& txn, const evmc_rev
 
     /* Should the sender already be present it means the validation of signature already occurred */
     if (!txn.from.has_value()) {
-        if (!is_special_signature(txn.r, txn.s) && !is_valid_signature(txn.r, txn.s, rev >= EVMC_HOMESTEAD)) {
+        // FIXME: enable has_homestead in some evm_version
+        //const bool has_homestead{rev >= EVMC_HOMESTEAD};
+        const bool has_homestead{false};
+
+        if (!is_special_signature(txn.r, txn.s) && !is_valid_signature(txn.r, txn.s, has_homestead)) {
             return ValidationResult::kInvalidSignature;
         }
     }
